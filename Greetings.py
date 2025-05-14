0443800090e5125ccddb621de9669d03fe7aceca
Yasinkerun <141756935+Yasinkerun@users.noreply.github.com>
2025-05-14 21:31:31 +0530

    Add files via upload

---
+++ Greetings.py
@@ -0,0 +1,6 @@
+FirstName = input("Enter First Name: ")
+LastName = input("Enter Last Name: ")
+
+FullName = FirstName + " " + LastName
+
+print("Hello " + FullName + "! Welcome to Python Program")
---
+++ Math_operations.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter First number: "))
+num2 = int(input("Enter Second number: "))
+Addition = num1 + num2
+Subtraction = num1 - num2
+Multiplication = num1 * num2
+Division = num1 / num2
+
+print("Addition: ", Addition)
+print("Subtraction: ", Subtraction)
+print("Multiplication: ", Multiplication)
+print("Division: ", Division)
