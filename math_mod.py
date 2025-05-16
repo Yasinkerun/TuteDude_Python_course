7423a0b2bd3d4b9e1592dd26e36a0aea470c22a3
Yasinkerun <141756935+Yasinkerun@users.noreply.github.com>
2025-05-16 19:34:53 +0530

    Add files via upload

---
+++ math_mod.py
@@ -0,0 +1,9 @@
+from math import *
+num = int(input("Enter a number: "))
+
+sqrt(num)
+log(num)
+sin(num)
+print("Square root of number is: ", sqrt(num))
+print("Logarithm of number is: ", log(num))
+print("Sine of number is: ", sin(num))
